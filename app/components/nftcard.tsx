9a14b9ff73cf5ed5c14db5edfbc3166491402d2f
tanay-sgl <tanay.sagrolikar@gmail.com>
2024-02-13 14:07:45 +1100

    update_nftcard0

--- app/components/nftcard.tsx
+++ app/components/nftcard.tsx
@@ -9,6 +9,20 @@ export const NftCard: React.FC<NftCardProps> = ({ nft }) => {
   return (
     <div className="border border-gray-300 rounded-md p-4">
       <div className="flex flex-col items-center">
+        <h3 className="text-sm overflow-hidden">{nft.name}</h3>
+        <h3 className="text-sm overflow-hidden">
+          {nft.image_url ? (
+            <Image src={nft.image_url} width="100" height="100" alt=":(" />
+          ) : (
+            <h3 className="text-sm overflow-hidden">No image</h3>
+          )}
+        </h3>
+      </div>
+    </div>
+  );
+};
+
+/*
         {Object.keys(nft).map((key) => {
           if (key !== 'image_url') {
             return (
@@ -33,7 +47,4 @@ export const NftCard: React.FC<NftCardProps> = ({ nft }) => {
             }
           }
         })}
-      </div>
-    </div>
-  );
-};
+*/
