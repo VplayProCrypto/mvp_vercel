911ca3c38ca05936020ff698015af4b1f27c3a82
tanay-sgl <tanay.sagrolikar@gmail.com>
2024-02-15 18:20:17 +1100

    socials

--- app/components/nftcard.tsx
+++ app/components/nftcard.tsx
@@ -22,13 +22,13 @@ export const NftCard: React.FC<NftCardProps> = ({ nft, price }) => {
 
   return (
     <div className="border border-gray-300 rounded-md p-4">
-      <h3 className="text-sm overflow-hidden">{nft.name}</h3>
-      <h3>{price}</h3>
       {nft.image_url ? (
         <Image src={nft.image_url} width="100" height="100" alt=":(" />
       ) : (
         <h3 className="text-sm overflow-hidden">No image</h3>
       )}
+      <h3 className="text-sm overflow-hidden">{nft.name}</h3>
+      <h3>{price}</h3>
     </div>
   );
 };
--- pages/game/[name].tsx
+++ pages/game/[name].tsx
@@ -56,10 +56,26 @@ const parsePrice = (currentPrice: CurrentListingPrice) => {
     : currentPrice.value + ' ' + currentPrice.currency;
 };
 
-const Game: React.FC<{ game: Collection }> = ({ game }) => {
+const Hero: React.FC<{ game: Collection }> = ({ game }) => {
   return (
     <div className="bg-white rounded-lg shadow-lg p-4 m-4">
-      <h3 className="text-6xl font-bold mb-2 mt-4">{game.name}</h3>
+      <div className="flex flex-row items-center">
+        <h3 className="text-6xl font-bold mb-2 mt-4">{game.name}</h3>
+        <div className="relative mt-4 mb-4">
+          {game.image_url ? (
+            <Image
+              src={game.image_url}
+              height={100}
+              width={100}
+              alt={game.name}
+              className="mt-4"
+            />
+          ) : (
+            <p className="text-sm text-gray-500 mt-4">No image</p>
+          )}
+        </div>
+      </div>
+
       <div className="relative mt-4 mb-4">
         {game.banner_image_url ? (
           <Image
@@ -70,11 +86,65 @@ const Game: React.FC<{ game: Collection }> = ({ game }) => {
             className="mt-4"
           />
         ) : (
-          <p className="text-sm text-gray-500 mt-4">No image</p>
+          <p className="text-sm text-gray-500 mt-4">No banner image</p>
         )}
       </div>
 
       <p className="text-1xl text-gray-600 mb-4">{game.description}</p>
+    </div>
+  );
+};
+
+interface SocialsProps {
+  game: Collection;
+}
+
+const Socials: React.FC<SocialsProps> = ({ game }) => {
+  return (
+    <div className="bg-white rounded-lg shadow-lg p-4 m-4">
+      <h3 className="text-3xl font-bold mb-2 mt-4">Socials</h3>
+      <a href={game.opensea_url} className="text-blue-500 hover:text-blue-700">
+        <button className="bg-blue-200 hover:bg-blue-500 text-white font-bold py-2 px-4 rounded m-1">
+          OpenSea
+        </button>
+      </a>
+      <a
+        href={game.project_url}
+        className="text-green-500 hover:text-green-700"
+      >
+        <button className="bg-green-200 hover:bg-green-500 text-white font-bold py-2 px-4 rounded m-1">
+          Project
+        </button>
+      </a>
+      <a href={game.wiki_url} className="text-yellow-500 hover:text-yellow-700">
+        <button className="bg-yellow-200 hover:bg-yellow-500 text-white font-bold py-2 px-4 rounded m-1">
+          Wiki
+        </button>
+      </a>
+      <a
+        href={game.discord_url}
+        className="text-indigo-500 hover:text-indigo-700"
+      >
+        <button className="bg-indigo-200 hover:bg-indigo-500 text-white font-bold py-2 px-4 rounded m-1">
+          Discord
+        </button>
+      </a>
+      <a
+        href={game.telegram_url}
+        className="text-purple-500 hover:text-purple-700"
+      >
+        <button className="bg-purple-200 hover:bg-purple-500 text-white font-bold py-2 px-4 rounded m-1">
+          Telegram
+        </button>
+      </a>
+      {/* Add more buttons for other properties as needed */}
+      <h3 className="text-xl font-bold text-gray-700 mt-2">
+        @{game.twitter_username}
+      </h3>
+      <h3 className="text-xl font-bold text-gray-700 mt-2">
+        {game.instagram_username}
+      </h3>
+      {/* Add more h3 tags for other properties as needed */}
     </div>
   );
 };
@@ -85,7 +155,9 @@ export default function Page({
 }: InferGetServerSidePropsType<typeof getServerSideProps>) {
   return (
     <main className="h-full bg-gray-50">
-      <Game game={collection} />
+      <Hero game={collection} />
+      <Socials game={collection} />
+
       <p className="text-2xl font-bold mb-2 mt-4">NFTS</p>
       <div className="grid grid-cols-6 gap-4 mt-4">
         {listings ? (
