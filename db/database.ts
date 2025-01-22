e509f806e16ef0c8859c823022e625c1a3cbf365
tanay-sgl <tanay.sagrolikar@gmail.com>
2025-01-23 01:30:38 +1100

    db_migration

--- db/database.ts
+++ db/database.ts
@@ -1,22 +1,25 @@
+import { readFileSync } from 'fs'
 import { Pool } from 'pg'
 import { env } from 'process'
 
-console.log('Database connection variables:', {
-  host: env.NEW_TIMESCALE_HOST,
-  port: env.NEW_TIMESCALE_PORT,
-  user: env.NEW_TIMESCALE_USER,
-  database: env.NEW_TIMESCALE_DATABASE,
-})
+import { join } from 'path'
 
 export const pool = new Pool({
-  host: env.NEW_TIMESCALE_HOST || 'localhost',
-  port: Number(env.NEW_TIMESCALE_PORT) || 5432,
-  user: env.NEW_TIMESCALE_USER || 'postgres',
-  password: env.NEW_TIMESCALE_PASSWORD || 'postgres',
-  database: env.NEW_TIMESCALE_DATABASE || 'postgres',
+  host: env.AWS_TIMESCALE_HOST || 'localhost',
+  port: Number(env.AWS_TIMESCALE_PORT) || 5432,
+  user: env.AWS_TIMESCALE_USER || 'postgres',
+  password: env.AWS_TIMESCALE_PASSWORD || 'postgres',
+  database: env.AWS_TIMESCALE_DATABASE || 'postgres',
   max: 20,
   idleTimeoutMillis: 3000,
-  connectionTimeoutMillis: 2000,
+  connectionTimeoutMillis: 10000,
+  ssl: {
+    rejectUnauthorized: false,
+    // cert: readFileSync(join(process.cwd(), 'db/cert.crt')).toString(),
+    // key: readFileSync(join(process.cwd(), 'db/key.key')).toString(),
+    cert: Buffer.from(env.SSL_CERT || '', 'base64').toString(),
+    key: Buffer.from(env.SSL_KEY || '', 'base64').toString(),
+  },
 })
 
 // When your application is shutting down
