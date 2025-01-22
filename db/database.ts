0c94c937af0fc1c4037bf305f4c0c8fccc0f003c
tanay-sgl <tanay.sagrolikar@gmail.com>
2025-01-23 01:47:49 +1100

    process_env

--- db/database.ts
+++ db/database.ts
@@ -5,27 +5,25 @@ import { env } from 'process'
 import { join } from 'path'
 
 console.log(
-  'env.AWS_TIMESCALE_HOST: ' + env.AWS_TIMESCALE_HOST,
-  'env.AWS_TIMESCALE_PORT: ' + env.AWS_TIMESCALE_PORT,
-  'env.AWS_TIMESCALE_USER: ' + env.AWS_TIMESCALE_USER,
-  'env.AWS_TIMESCALE_DATABASE: ' + env.AWS_TIMESCALE_DATABASE
+  'env.AWS_TIMESCALE_HOST: ' + process.env.AWS_TIMESCALE_HOST,
+  'env.AWS_TIMESCALE_PORT: ' + process.env.AWS_TIMESCALE_PORT,
+  'env.AWS_TIMESCALE_USER: ' + process.env.AWS_TIMESCALE_USER,
+  'env.AWS_TIMESCALE_DATABASE: ' + process.env.AWS_TIMESCALE_DATABASE
 )
 
 export const pool = new Pool({
-  host: env.AWS_TIMESCALE_HOST || 'localhost',
-  port: Number(env.AWS_TIMESCALE_PORT) || 5432,
-  user: env.AWS_TIMESCALE_USER || 'postgres',
-  password: env.AWS_TIMESCALE_PASSWORD || 'postgres',
-  database: env.AWS_TIMESCALE_DATABASE || 'postgres',
+  host: process.env.AWS_TIMESCALE_HOST || 'localhost',
+  port: Number(process.env.AWS_TIMESCALE_PORT) || 5432,
+  user: process.env.AWS_TIMESCALE_USER || 'postgres',
+  password: process.env.AWS_TIMESCALE_PASSWORD || 'postgres',
+  database: process.env.AWS_TIMESCALE_DATABASE || 'postgres',
   max: 20,
   idleTimeoutMillis: 3000,
   connectionTimeoutMillis: 10000,
   ssl: {
     rejectUnauthorized: false,
-    // cert: readFileSync(join(process.cwd(), 'db/cert.crt')).toString(),
-    // key: readFileSync(join(process.cwd(), 'db/key.key')).toString(),
-    cert: Buffer.from(env.SSL_CERT || '', 'base64').toString(),
-    key: Buffer.from(env.SSL_KEY || '', 'base64').toString(),
+    cert: Buffer.from(process.env.SSL_CERT || '', 'base64').toString(),
+    key: Buffer.from(process.env.SSL_KEY || '', 'base64').toString(),
   },
 })
 
